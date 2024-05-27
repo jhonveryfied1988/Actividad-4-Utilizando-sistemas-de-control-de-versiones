d2573fbdb4e328b9162de849b8a0af2f911f8e1f
Jhon Alexander Perez Ramirez <jhonveryfied1988@gmail.com>
2024-05-26 20:46:20 -0500

    Historia 2: Inicio de Sesión Descripción: Como usuario, quiero poder iniciar sesión en la aplicación proporcionando mi correo electrónico y contraseña para acceder a mi cuenta.
    Criterios de Aceptación:
    
    El usuario debe proporcionar un correo electrónico y una contraseña válidos.
    Debe mostrarse un mensaje de error si las credenciales son incorrectas.
    Debe generarse un token JWT si las credenciales son correctas.
    Requisitos Técnicos:
    
    Implementar un controlador para el inicio de sesión.
    Añadir una ruta para manejar la solicitud de inicio de sesión.
    Manejar intentos fallidos y bloquear al usuario después de 3 intentos fallidos.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -16,3 +16,32 @@ exports.register = async(req, res) => {
         res.status(500).json({ message: 'Server error', error });
     }
 };
+
+
+exports.login = async(req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user || user.isLocked) {
+            return res.status(400).json({ message: 'Invalid credentials or user is locked' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            user.failedLoginAttempts += 1;
+            if (user.failedLoginAttempts >= 3) {
+                user.isLocked = true;
+            }
+            await user.save();
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        user.failedLoginAttempts = 0;
+        await user.save();
+
+        const token = jwt.sign({ userId: user._id }, 'secretKey', { expiresIn: '1h' });
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+};
